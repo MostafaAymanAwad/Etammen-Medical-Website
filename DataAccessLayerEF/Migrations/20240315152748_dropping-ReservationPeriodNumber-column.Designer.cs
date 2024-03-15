d0a8e21845ea08353c60cf91eb26b8cbe7b74de4
Moaz <moazsamy333@gmail.com>
2024-03-15 17:44:26 +0200

    Modified ReservationPeriodNumber column

--- DataAccessLayerEF/Migrations/20240315152748_dropping-ReservationPeriodNumber-column.Designer.cs
+++ DataAccessLayerEF/Migrations/20240315152748_dropping-ReservationPeriodNumber-column.Designer.cs
@@ -5,6 +5,7 @@ using DataAccessLayerEF.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,9 +13,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayerEF.Migrations
 {
     [DbContext(typeof(EtammenDbContext))]
-    partial class EtammenDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315152748_dropping-ReservationPeriodNumber-column")]
+    partial class droppingReservationPeriodNumbercolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -123,12 +126,6 @@ namespace DataAccessLayerEF.Migrations
 
                     b.Property<bool>("IsDeleted")
                         .HasColumnType("bit");
-
-                    b.Property<bool>("IsHomeVisit")
-                        .HasColumnType("bit");
-
-                    b.Property<TimeOnly>("ReservationPeriodNumber")
-                        .HasColumnType("time");
 
                     b.Property<int>("patientId")
                         .HasColumnType("int");
